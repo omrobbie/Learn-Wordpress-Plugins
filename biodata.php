757a6e6b06e01a756e4b9a2ed8b17d2690985aa0
omrobbie <putu.roby@yahoo.com>
2017-04-13 11:05:58 +0800

    membuat identitas plugins

---
+++ biodata.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: Biodata
+Plugin URI: https://github.com/omrobbie/learn-wordpress-plugins.git
+Description: Menampilkan biodata guru di halaman pengunjung
+Version: 1.0
+Author: omrobbie
+Author URI: http://omrobbie.com
+ */
